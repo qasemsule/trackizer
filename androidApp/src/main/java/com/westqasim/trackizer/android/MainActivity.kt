a64ea0c3582e91dd12f4fd7b7843fe083c2627a9
qasemsule <devqasimsule@gmail.com>
2024-07-22 21:43:57 -0700

    update gradle

--- androidApp/src/main/java/com/westqasim/trackizer/android/MainActivity.kt
+++ androidApp/src/main/java/com/westqasim/trackizer/android/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.westqasim.trackizer.Greeting
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +19,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-///working
+
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+
     }
 }

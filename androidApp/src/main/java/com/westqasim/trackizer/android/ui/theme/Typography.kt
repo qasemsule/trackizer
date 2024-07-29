4a3b9d770676d3e228cbd6d24c95835069841871
qasemsule <devqasimsule@gmail.com>
2024-07-28 22:33:24 -0700

    typography updated

---
+++ androidApp/src/main/java/com/westqasim/trackizer/android/ui/theme/Typography.kt
@@ -0,0 +1,17 @@
+package com.westqasim.trackizer.android.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+
+val MyTypography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 72.sp,
+        lineHeight = 108.sp
+    ),
+)

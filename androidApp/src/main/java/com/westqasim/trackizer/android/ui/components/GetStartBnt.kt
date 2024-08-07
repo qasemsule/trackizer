67c1c4083d5774a44c6d85e2d52519f0b8786686
qasemsule <devqasimsule@gmail.com>
2024-08-07 12:59:46 -0700

    update WelcomeScreen

--- androidApp/src/main/java/com/westqasim/trackizer/android/ui/components/GetStartBnt.kt
+++ androidApp/src/main/java/com/westqasim/trackizer/android/ui/components/GetStartBnt.kt
@@ -1,31 +1,62 @@
 package com.westqasim.trackizer.android.ui.components
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.westqasim.trackizer.android.MyApplicationTheme
+import com.westqasim.trackizer.android.ui.theme.Colors
 import com.westqasim.trackizer.android.ui.theme.MyTypography
 
 
 @Composable
-
-fun GetStartBtn(text : String, color : Color){
+fun GetStartBtn(text : String, textColor :Color, color : Color){
     // Rounded Button
-    Button(
-        onClick = { /*TODO*/ },
-        modifier = Modifier.padding(8.dp).fillMaxWidth(),
-        shape = RoundedCornerShape(964.dp),
-        colors = ButtonDefaults.buttonColors(color),
-    ) {
-        Text(text = text, fontSize = 14.sp, fontFamily = MyTypography.labelLarge.fontFamily)
+    Box{
+        Button(
+            onClick = { /*TODO*/},
+            modifier = Modifier
+                .width(324.dp),
+            shape = RoundedCornerShape(964.dp),
+            colors = ButtonDefaults.buttonColors(color),
+            elevation = ButtonDefaults.buttonElevation(2.dp),
+            ) {
+            Text(
+                text = text,
+                fontSize = 14.sp,
+                fontFamily = MyTypography.labelLarge.fontFamily,
+                color = textColor)
+        }
     }
+
+}
+
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    GetStartBtn(
+        text = "Get Started",
+        textColor = Colors.Gray.white,
+        color = Colors.AccentPrimary.accentP100
+    )
 }
 
 
--- androidApp/src/main/java/com/westqasim/trackizer/android/ui/screens/WelcomeScreen.kt
+++ androidApp/src/main/java/com/westqasim/trackizer/android/ui/screens/WelcomeScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,12 +21,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.westqasim.trackizer.android.R
 import com.westqasim.trackizer.android.ui.components.FrameAnimation
 import com.westqasim.trackizer.android.ui.components.GetStartBtn
 import com.westqasim.trackizer.android.ui.theme.Colors
+import com.westqasim.trackizer.android.ui.theme.MyTypography
+import kotlin.math.ceil
 
 @Composable
 fun WelcomeScreen() {
@@ -33,15 +41,40 @@ fun WelcomeScreen() {
                     .fillMaxSize() //fill max width of the screen
                     .background(Colors.Gray.gray80),
                ){
-                Column() {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(
                         modifier = Modifier
-                            .fillMaxWidth()
                             .padding(top = 60.dp, bottom = 59.dp),
                         painter = painterResource(id = R.drawable.logo),
                         contentDescription = "Logo")
 
                     FrameAnimation()
+
+                    Text(
+                        text = "Congue malesuada in ac justo, a tristique leo massa. Arcu leo leo urna risus." ,
+                        fontFamily = MyTypography.labelSmall.fontFamily,
+                        color = Colors.Gray.white,
+                        fontSize = 14.sp,
+                        lineHeight = 20.sp,
+                        letterSpacing = 0.2.sp,
+                        modifier = Modifier.width(290.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    GetStartBtn(
+                        text = "Get Started",
+                        textColor = Colors.Gray.white,
+                        color = Colors.AccentPrimary.accentP100,
+                    )
+                    GetStartBtn(
+                        text = "I have an account",
+                        textColor = Colors.Gray.white,
+                        color = Colors.Gray.gray70
+                    )
+
                 }
             }
 
